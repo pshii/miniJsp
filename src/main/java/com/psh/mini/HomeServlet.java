bcb8716fc44cc8b60c02d68b4b33703c7b214c55
pshii <sjsmssk2@naver.com>
2022-02-20 16:13:26 +0900

    servlet으로 jsp response

---
+++ src/main/java/com/psh/mini/HomeServlet.java
@@ -0,0 +1,32 @@
+package com.psh.mini;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public HomeServlet() {
+        super();
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/index.jsp");
+		requestDispatcher.forward(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
